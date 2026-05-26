8eb118421a303c06af2e21144753ad4259476f44
hanafitit <maremaret85@gmail.com>
2026-05-26 22:30:56 +0500

    Update DomainModels.cs

--- TestApp/DomainModels.cs
+++ TestApp/DomainModels.cs
@@ -23,7 +23,7 @@ internal class DomainModels
     {
         public long FavoriteChatId { get; set; }       // ID чата «Избранное», где принимаются команды
         public long OwnerUserId = 7499022252;          // ваш пользовательский ID в Telegram
-        public string? NeuralApiToken { get; set; } = "";    // токен для API нейросети уникализации
+           // токен для API нейросети уникализации
         public bool UseSendChatAction { get; set; } = true; // использовать ли SendChatAction перед отправкой
     }
 
